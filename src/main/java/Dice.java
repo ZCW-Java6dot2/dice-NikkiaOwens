10f96d8be98f992ab5870a69ce6a5f49a4c652f1
nikki <nikki@Gbrads-Mac.local>
2020-11-11 22:18:36 -0500

    Finished simulataion and added results file

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,36 @@
+import java.util.Random;
+
 public class Dice {
 
+public Integer numberOfDies;
+public Integer numberOfTosses;
+
+public Dice (Integer numberOfDies, Integer numberOfTosses) {
+    this.numberOfDies= numberOfDies;
+    this.numberOfTosses = numberOfTosses;
+
+
+}
+
+
+
+    public  Integer tossAndSum() {
+
+    Integer sum = 0;
+   // int maxRoll = 6;
+   // int minRoll = 1;
+    Random random = new Random();
+
+
+    for(int die = 0; die < numberOfDies; die++) {
+
+    sum += random.nextInt(6) + 1;
+}
+    return sum;
+}
+
+public Integer getDiceRolled(){
+    return this.numberOfDies;
+}
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,38 @@
-public class Simulation {
 
+    public class Simulation {
+        Integer numberOfDies;
+        Integer numberOfTosses;
+        Integer binLow;
+        Integer binHigh;
+        Bins bin;
+        public Simulation(Integer numberOfDies, Integer numberOfTosses) {
+            this.numberOfDies = numberOfDies;
+            this.numberOfTosses = numberOfTosses;
+            this.binLow = numberOfDies;
+            this.binHigh = numberOfDies * 6;
+            this.bin = new Bins(binLow, binHigh, numberOfTosses);
+        }
+        public static void main(String[] args) {
+            Console console = new Console();
+            int numberOfDies= 0;
+            int numberOfTosses = 0;
+            numberOfDies = console.getIntegerInput("\u001B[34m Please enter Number of Dice:\u001B[34m").intValue();
+            numberOfTosses = console.getIntegerInput("\u001B[34m Please enter Number of Tosses:\u001B[34m\u001B[0m").intValue();
+            Simulation simulateToss = new Simulation(numberOfDies, numberOfTosses);
+            simulateToss.runSimulation();
+        }
+        public void runSimulation() {
+            System.out.println("***" + "\n" + "Simulation of " + numberOfDies + " dice tossed for " + numberOfTosses + " times" + "\n" + "***");
+            Dice rollTheDice = new Dice(this.numberOfDies,this.numberOfTosses);
 
+            //   Integer outputOfToss = rollTheDice.tossAndSum();
+            // System.out.println(outputOfToss);
+            //  outputOfToss = rollTheDice.tossAndSum();
+            //sum+=Math.random()*(max-min+1)+min;
+            for (Integer i = 0; i < this.numberOfTosses; i++) {
+                bin.incrementBin(rollTheDice.tossAndSum());
 
-}
+            }
+            System.out.println(bin.toString());
+        }
+    }
